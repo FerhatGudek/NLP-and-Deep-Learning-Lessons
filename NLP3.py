c75881ba40155f204c28eb19e7967bf7acc6f969
Ferhat Güdek <ferhat.gudek10@gmail.com>
2025-11-22 12:34:40 +0300

    Add files via upload

---
+++ NLP3.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 21 23:50:07 2025
+
+@author: ferha
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+
+yorumlar = pd.read_csv("restaurant_comments.csv")
+print(yorumlar)
+
+yorum = re.sub("[^a-zA-Z]"," ",yorumlar["Review"][3]) # a dan z dışındakileri boşluk karakter ile değiştir (sıfırıncı satır için yaptı)
+               
+yorum = yorum.lower() # 3. işareti metin dizisi küçük harflere döndü
+yorum = yorum.upper() # 3. işaretli metin dizisi büyük harflere dönüştü
+yorum = yorum.split() # 3. işaretli metin dizisi bölünür (kelime kelime)
+print(yorum)
+
+import nltk
+nltk.download("stopwords")
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+
+derlem = []
+
+for i in range(len(yorumlar)):
+    yorum = re.sub("[^a-zA-Z]"," ",yorumlar["Review"][i])
+    yorum = yorum.lower()
+    yorum = yorum.split()
+    yorum = [ps.stem(kelime) for kelime in yorum if not kelime in set(stopwords.words("english"))]
+    yorum = " ".join(yorum)
+    derlem.append(yorum)
+    
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 2000)
+X = cv.fit_transform(derlem).toarray() # bağımsız değişken
+y = yorumlar.iloc[:,1].values # bağımlı değişken
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.20)
+
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(X_train,y_train)
+y_pred = gnb.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+   
